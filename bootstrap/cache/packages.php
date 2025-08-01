08b11c1210c182de92eafd213d247ed4eaa0d917
debasish1708 <debasishdas1708@gmail.com>
2025-08-01 21:53:16 +0530

    add passport for authentication

--- bootstrap/cache/packages.php
+++ bootstrap/cache/packages.php
@@ -13,6 +13,13 @@
       0 => 'Laravel\\Jetstream\\JetstreamServiceProvider',
     ),
   ),
+  'laravel/passport' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Passport\\PassportServiceProvider',
+    ),
+  ),
   'laravel/sail' => 
   array (
     'providers' => 
